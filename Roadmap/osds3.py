d44219dbd0601e318d214b5c218bb617d08deb44
osds3 <167836679+osds3@users.noreply.github.com>
2024-05-13 19:32:48 -0500

    Create osds3.py

---
+++ Roadmap/osds3.py
@@ -0,0 +1,18 @@
+CONSTANTE="constante"
+CONSTANTE="reconstante"
+print(CONSTANTE)
+
+#datos primitivos
+my_int=1
+my_float=1.5
+my_bool=True
+my_bool=False
+my_string="hola"
+
+texto="hola,python"
+print(texto)
+
+print(my_int)
+print(my_bool)
+print(my_float)
+print(my_string)
